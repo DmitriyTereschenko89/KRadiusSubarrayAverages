cccc0ca478f8e801f0149a805685a65464cbe94c
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-20 14:17:05 +0300

    Добавьте файлы проекта.

---
+++ KRadiusSubarrayAverages.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KRadiusSubarrayAverages
+{
+	internal class KRadiusSubarrayAverages
+	{
+		public int[] GetAverages(int[] nums, int k)
+		{
+			int n = nums.Length;
+			int[] averages = new int[n];
+			Array.Fill(averages, -1);
+			int slidingWindowSum = 0;
+			int slidingWindowLength = k * 2 + 1;
+			if (slidingWindowLength > n)
+			{
+				return averages;
+			}
+			for (int i = 0; i < slidingWindowLength; ++i)
+			{
+				slidingWindowSum += nums[i];
+			}
+			averages[k] = slidingWindowSum / slidingWindowLength;
+			for (int i = slidingWindowLength; i < n; ++i)
+			{
+				slidingWindowSum = slidingWindowSum - nums[i - slidingWindowLength] + nums[i];
+				averages[i - k] = slidingWindowSum / slidingWindowLength;
+			}
+			return averages;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace KRadiusSubarrayAverages
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			KRadiusSubarrayAverages averages = new();
+            Console.WriteLine(string.Join(", ", averages.GetAverages(new int[] { 7, 4, 3, 9, 1, 8, 5, 2, 6 }, 3)));
+			Console.WriteLine(string.Join(", ", averages.GetAverages(new int[] { 100000 }, 0)));
+            Console.WriteLine(string.Join(", ", averages.GetAverages(new int[] { 8 }, 100000)));
+        }
+	}
+}
